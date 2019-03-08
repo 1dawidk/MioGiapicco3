3f7ce346be8ce61151770a6fedaff438bd3bcc64
Dawid Kulpa <1dawidk@gmail.com>
2019-03-08 02:03:22 +0100

    image pusher debug log off

--- ImagePusher.cpp
+++ ImagePusher.cpp
@@ -18,40 +18,37 @@ void ImagePusher::onRun() {
     if(triggerCnt==0) {
         cv::Mat imgBuff;
 
-        cout << "Take a photo" << endl;
-
+        // Take a photo
         camera->getImage(&imgBuff);
         cv::imwrite("./imgs/lastshot.jpg", imgBuff);
         Thread::pause(500);
 
-        cout << "Done" << endl;
 
-        cout << "Create curl handle" << endl;
+        // Create curl handle
         CURL *curlHandle = curl_easy_init();
         curl_httppost *post=NULL;
         curl_httppost *lastpost=NULL;
 
 
-        cout << "Create http header" << endl;
+        // Create http header
         struct curl_slist *headerlist = NULL;
         headerlist = curl_slist_append(headerlist, "Content-Type: multipart/form-data");
 
-        cout << "Prepare post form" << endl;
+        // Prepare post form
         curl_formadd(&post, &lastpost,
                      CURLFORM_COPYNAME, "file",
                      CURLFORM_FILE, "./imgs/lastshot.jpg",
                      CURLFORM_END);
 
-        cout << "Set curl opts" << endl;
+        // Set curl opts
         curl_easy_setopt(curlHandle, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curlHandle, CURLOPT_HTTPHEADER, headerlist);
         curl_easy_setopt(curlHandle, CURLOPT_HTTPPOST, post);
         curl_easy_setopt(curlHandle, CURLOPT_VERBOSE, 1L);
 
-        cout << "Curl perform [ ... ]" << endl;
-        //curl_easy_perform(curlHandle);
+        // Perform curl
+        curl_easy_perform(curlHandle);
 
-        cout << "Curl perform [ DONE ]" << endl;
         triggerCnt= IMAGEPUSHER_TRIGGER_INITVALUE;
     } else {
         triggerCnt--;
