65994b4ddc749df9325a74256c5cfdc6123f7186
Dawid Kulpa <1dawidk@gmail.com>
2019-03-09 02:12:08 +0100

    data gatherer watch data valid

--- DataGatherer.cpp
+++ DataGatherer.cpp
@@ -14,7 +14,7 @@ DataGatherer::DataGatherer(const string &url,
 }
 
 void DataGatherer::onStart() {
-
+    lastSentHour=0;
 }
 
 void DataGatherer::onRun() {
@@ -28,33 +28,35 @@ void DataGatherer::onRun() {
         int water= wateringController->getMinLeft();
         int wind= windController->getState();
 
-        string curlPostFields="hum="+to_string(hum);
-        curlPostFields+="&temp="+to_string(temp);
-        curlPostFields+="&sun="+to_string(sun);
-        curlPostFields+="&water=";
-        if(water)
-            curlPostFields+= "1";
-        else
-            curlPostFields+= "0";
-        curlPostFields+="&wind=";
-        if(wind)
-            curlPostFields+="1";
-        else
-            curlPostFields+="0";
+        if(hum!=0 && temp!=0) {
+            string curlPostFields = "hum=" + to_string(hum);
+            curlPostFields += "&temp=" + to_string(temp);
+            curlPostFields += "&sun=" + to_string(sun);
+            curlPostFields += "&water=";
+            if (water)
+                curlPostFields += "1";
+            else
+                curlPostFields += "0";
+            curlPostFields += "&wind=";
+            if (wind)
+                curlPostFields += "1";
+            else
+                curlPostFields += "0";
 
-        CURL *curlHandle = curl_easy_init();
+            CURL *curlHandle = curl_easy_init();
 
-        // Set curl opts
-        curl_easy_setopt(curlHandle, CURLOPT_URL, url.c_str());
-        curl_easy_setopt(curlHandle, CURLOPT_POST, 1);
-        curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, "");
-        curl_easy_setopt(curlHandle, CURLOPT_VERBOSE, 0);
+            // Set curl opts
+            curl_easy_setopt(curlHandle, CURLOPT_URL, url.c_str());
+            curl_easy_setopt(curlHandle, CURLOPT_POST, 1);
+            curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, "");
+            curl_easy_setopt(curlHandle, CURLOPT_VERBOSE, 0);
 
-        // Perform curl
-        curl_easy_perform(curlHandle);
-        curl_easy_cleanup(curlHandle);
+            // Perform curl
+            curl_easy_perform(curlHandle);
+            curl_easy_cleanup(curlHandle);
 
-        lastSentHour= h;
+            lastSentHour = h;
+        }
     }
 
     Thread::pause(60000);
