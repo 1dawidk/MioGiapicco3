73e13b90e5ba016c9570e0470130f33c59c15f18
Dawid Kulpa <1dawidk@gmail.com>
2019-03-10 03:58:51 +0100

    sun controller set state bug fix, pwm output

--- SunController.cpp
+++ SunController.cpp
@@ -3,19 +3,27 @@
 
 void SunController::onStart() {
     int dayM= Clock::GetDayMinutes();
-    int h= dayM/60;
+    int hh= dayM/30;
 
-    state= sunAtHalfHour[h];
+    state= sunAtHalfHour[hh];
+
+    bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_12, BCM2835_GPIO_FSEL_ALT5);
+    bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_16);
+    bcm2835_pwm_set_mode(0, 1, 1);
+    bcm2835_pwm_set_range(0, 1000);
 }
 
 void SunController::onRun() {
     int dayM= Clock::GetDayMinutes();
-    int h= dayM/60;
+    int hh= dayM/30;
 
-    if(state<sunAtHalfHour[h])
+    if(state<sunAtHalfHour[hh]) {
         state++;
-    else if(state>sunAtHalfHour[h])
+        bcm2835_pwm_set_data(0, (uint32_t)state*10);
+    } else if(state>sunAtHalfHour[hh]) {
         state--;
+        bcm2835_pwm_set_data(0, (uint32_t)state*10);
+    }
 
     Thread::pause(120000);
 }
