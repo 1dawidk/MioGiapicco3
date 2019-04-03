85dbfda1df65a1977bb07030e89d6665e3c6b2a9
Dawid Kulpa <1dawidk@gmail.com>
2019-04-03 20:00:40 +0200

    MCP3208 log fix

--- Devices/MCP3208.cpp
+++ Devices/MCP3208.cpp
@@ -19,7 +19,7 @@ float MCP3208::readCh(uint8_t ch) {
     vol=(uint16_t)( ((uint16_t)(buff[1])<<6) | (buff[2]>>2));
     vol=vol*(vRef/4095);
 
-    std::cout << "Spi out: " << std::to_string(buff[0]) << " " << std::to_string(buff[1]) << " " << std::to_string(buff[2]) << ", V: " << vol << std::endl;
+    std::cout << "Spi out (ch: "<< ch << ": " << std::to_string(buff[0]) << " " << std::to_string(buff[1]) << " " << std::to_string(buff[2]) << ", V: " << vol << std::endl;
 
     return vol;
 }
